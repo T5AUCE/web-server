16add047e36a234918205928dd0433e77a920a04
liudyl <dyliu@umass.edu>
2026-09-11 16:46:42 +0000

    initial Express server with two routes

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ app.get("/", (req, res) => {
   res.send("Hello, web!");
 });
 
-app.get("/about", (req, res) => {
-  res.send("This is a web programming course.");
-});
-
 const projects = [
   { name: "Weather app", tag: "javascript" },
   { name: "Portfolio site", tag: "express" },

7169b0eafb56e28974b77e231178e68a6cfcfd3c
liudyl <dyliu@umass.edu>
2026-09-25 16:26:58 +0000

    random-stuff

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = 3000;
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static("public"));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello, web!");
@@ -30,9 +31,43 @@ app.get("/projects", (req, res) => {
   }
 });
 
+const events = [
+  { title: "First", body: "Blah blah blah 1." },
+  { title: "Second", body: "Blah blah blah 2." },
+];
+
 app.get("/events", (req, res) => {
-  const events = [];
   res.render("events", { title: "Events", events });
+});
+
+app.delete("/events/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (Number.isNaN(id) || id < 0 || id >= events.length) {
+    res.status(404).json({ error: "Entry not found" });
+    return;
+  }
+  events.splice(id, 1);
+  res.status(204).send();
+});
+
+const wishlist = [
+  { title: "First", body: "Blah blah blah 1." },
+  { title: "Second", body: "Blah blah blah 2." },
+];
+
+app.get("/wishlist", (req, res) => {
+  return res.json(wishlist);
+});
+
+app.post("/wishlist", (req, res) => {
+  const { item, note } = req.body;
+  // your decision goes here
+  if (!item || !note) {
+    return res.status(400).json({ error: "Item and note are required." });
+  }
+  const newItem = { item, note };
+  wishlist.push(newItem);
+  res.status(201).json(newItem);
 });
 
 app.listen(PORT, () => {
